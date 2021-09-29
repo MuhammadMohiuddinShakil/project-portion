7bbcda895e864fb680a380641ee52ab52a2408bd
[MuhammadMohiuddinShakil] <[mmohiuddin172203@bscse.uiu.ac.bd]>
2021-09-29 07:29:29 +0600

    icon and background on nav bar

---
+++ add_input.php
@@ -0,0 +1,22 @@
+<h1>Create Record</h1>
+
+
+
+<form method=get action=add_result.php>
+
+	doctor_name: <input type=text name=doctor_name> <br>
+
+	<p>
+
+	patient_name: <input type=text name=patient_name> <br>
+
+	<p>
+
+    date: <input type=date name=date value=''>
+
+	<p>
+
+
+	<input type=submit value=Insert>
+
+</form>
